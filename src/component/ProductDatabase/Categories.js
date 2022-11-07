22db4b4201cc562da5bc314d8d19e1c17ea5470b
Aman Mulani <mulaniman0504@gmail.com>
2022-11-07 19:53:37 +0530

    added new image and worked on categories

--- src/component/ProductDatabase/Categories.js
+++ src/component/ProductDatabase/Categories.js
@@ -1,26 +1,28 @@
-export default Categories = [
+  const Categories = [
   {
     id: 1,
     CategoryName: "Shirt",
     CategoryImage:
-      "https://m.media-amazon.com/images/I/51TauPInDOL._AC_UL480_FMwebp_QL65_.jpg",
+      "https://www.muftijeans.in/media/home2022/oct22/newarrival-shirts-1_ioNw0ze0qL.jpg",
   },
   {
     id: 2,
     CategoryName: "Jeans",
     CategoryImage:
-      "https://m.media-amazon.com/images/I/71J5v1vrKaL._UX679_.jpg",
+      "https://www.muftijeans.in/media/home2022/oct22/newarrival-jeans_D9yHmPDp7.jpg",
   },
   {
     id: 3,
     CategoryName: "T-shirt",
     CategoryImage:
-      "https://m.media-amazon.com/images/I/61LosQ+pz6L._AC_UL480_FMwebp_QL65_.jpg",
+      "https://www.muftijeans.in/media/home2022/oct22/newarrival-tshirts_gkno9ZAxY.jpg",
   },
   {
     id: 4,
     CategoryName: "Jackets and Hoodies",
     CategoryImage:
-      "https://m.media-amazon.com/images/I/318TYiCcI1L._AC_UL480_FMwebp_QL65_.jpg",
+      "https://www.muftijeans.in/media/home2022/oct22/newarrival-jackets_6LINjSLok.jpg",
   },
 ];
+
+export default Categories
--- src/component/main/Main.jsx
+++ src/component/main/Main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "../header/Header";
 import MainImg from "../assets/MUFTI3.png";
 import { useNavigate } from "react-router-dom";
+import Categories from '../ProductDatabase/Categories'
 import "./main.css";
 const main = () => {
   const navigate = useNavigate();
@@ -13,7 +14,7 @@ const main = () => {
       <br />
 
       <div className="MainImg">
-        <img src={MainImg}></img>
+        <img src="https://lapol0.s3.amazonaws.com/media/None/100-years-of-men-fashion-cover-lapolo.jpg"></img>
         {/* <span>Here the All TrenZZZ are...</span> */}
 
         <div className="BtnContainer">
@@ -26,6 +27,28 @@ const main = () => {
             Explore TrendZZZ
           </button>
         </div>
+      </div>
+      <div className="categoryHeading">
+        <h3>Top Categories</h3>
+      </div>
+      <div className="maincategories">
+        {Categories.map((item)=>{
+          return(
+            <div>
+            <div className="CategoryCard">
+              <img
+                className="CategoryImage"
+                src={item.CategoryImage}
+              ></img>
+              <div className="middle">
+              <h5 className="CategoryName">{item.CategoryName}</h5>
+
+              </div>
+            </div>
+          </div>
+
+          )
+        })}
       </div>
     </>
   );
